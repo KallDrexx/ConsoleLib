f2315ae952a617e859966a15ec64452229ee2e45
KallDrexx <me@mshapiro.net>
2012-08-05 01:09:07 -0400

    Added test project to test processors

---
+++ TestConsoleHost/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConsoleLib.Console;
+using ConsoleLib.Console.Networking;
+using System.Threading;
+
+namespace TestConsoleHost
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var test = new TestUtils();
+
+            // Initialize the console
+            var sourceObj = new object();
+            ConsoleManager.Instance.InitializeProcessor(new JavascriptConsoleProcessor());
+            ConsoleManager.Instance.RegisterObjectToConsole(sourceObj, "test", test);
+            var host = new ConsoleServiceHost("localhost", 4000);
+
+            Console.WriteLine("Server started, waiting for commands");
+
+            while (!test.Quit)
+            {
+                // Keep looping until quit has been called
+                Thread.Sleep(100);
+            }
+
+            ConsoleManager.Instance.UnregisterObjectsFromSource(sourceObj);
+        }
+    }
+}
---
+++ TestConsoleHost/TestUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConsoleLib.Console;
+
+namespace TestConsoleHost
+{
+    public class TestUtils
+    {
+        public bool Quit { get; set; }
+
+        public void Print(string message)
+        {
+            ConsoleManager.Instance.AddOutput(message);
+        }
+    }
+}
